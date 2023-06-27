c1622d41e5c6c8624a24700b4c090bc7837984fb
Jayslen <jayslen134@gmail.com>
2023-06-27 17:39:58 -0400

    updated

--- src/components/JobsLabel.jsx
+++ src/components/JobsLabel.jsx
@@ -4,7 +4,7 @@ import { JobsLabelTools } from './JobsLabelTools'
 // Job Label LI Elm.
 function JobsLabel ({ job, methods }) {
   return (
-    <li className='w-full flex flex-col bg-white shadow-2xl rounded-md px-2 py-4 pt-8 relative md:flex-row md:items-center md:justify-between md:pt-4'>
+    <li className={`w-full flex flex-col bg-white shadow-2xl rounded-md px-2 py-4 pt-8 relative md:flex-row md:items-center md:justify-between md:pt-4 ${job.featured ? 'border-l-4 border-l-[#458586]' : null}`}>
       <article className='flex items-center gap-2'>
         <img src={job.logo} alt={job.company + 'logo'} className='w-12 h-12 absolute -top-5 md:static md:w-16 md:h-16' />
         <JobListHeader job={job} />
